0b8617086308cca28a33d19a950466df2847584f
Mark Moussa <mark.m.moussa@nasa.gov>
2021-02-24 12:01:07 -0500

    fjkghh'

--- esrgan_testing.py
+++ esrgan_testing.py
@@ -5,13 +5,12 @@ from ISR.models import RRDN
 image_name = "biosphere7_mollweide.1505.tif"
 img = Image.open('images_test/' + image_name)
 lr_img = np.array(img)
-
 rrdn = RRDN(weights="gans")
 sr_img = rrdn.predict(lr_img)
 sr_img_cleaned = Image.fromarray(sr_img)
 sr_img_cleaned.save("test_output/" + image_name + "_upscaled_RRDN.tif")
 
-bicubic_img = lr_img.resize(size=(lr_img.size[0]*2, lr_img.size[1]*2), resample=Image.BICUBIC)
+bicubic_img = lr_img.resize(size=(img.size[0]*2, img.size[1]*2), resample=Image.BICUBIC)
 bicubic_img.save("test_output/" + image_name + "_bicubic.tif")
 
 
